1d886e9dfd048be3c9336692316802c0f377e954
Ana Carolina <ana.sferreira@catho.com>
2021-04-18 12:00:46 -0300

    Initial Commit

---
+++ firebase-login-swiftui/firebase_login_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebase_login_swiftuiApp.swift
+//  firebase-login-swiftui
+//
+//  Created by Ana Carolina de Sousa Ferreira on 18/04/21.
+//
+
+import SwiftUI
+
+@main
+struct firebase_login_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
